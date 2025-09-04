afacc1296eb30d41424814aad69c8c3f83d12d22
Abhishek Mandingi <mabhi256@gmail.com>
2025-09-04 06:41:59 -0400

    #EB4 Decode bencoded integers

--- app/main.go
+++ app/main.go
@@ -9,36 +9,72 @@ import (
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
+func decodeBencode(bencodedString string) (any, error) {
+	beginDelimiter := bencodedString[0]
+	endDelimiter := bencodedString[len(bencodedString)-1]
 
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
+	switch {
+	case unicode.IsDigit(rune(beginDelimiter)):
+		return decodeString(bencodedString)
 
-		lengthStr := bencodedString[:firstColonIndex]
+	case string(beginDelimiter) == "i" && string(endDelimiter) == "e":
+		return decodeInteger(bencodedString)
 
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
+	default:
+		return "", fmt.Errorf("format not supported")
 
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else {
-		return "", fmt.Errorf("only strings are supported at the moment")
 	}
+}
+
+func decodeString(bencodedString string) (any, error) {
+	var firstColonIndex int
+
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == ':' {
+			firstColonIndex = i
+			break
+		}
+	}
+
+	lengthStr := bencodedString[:firstColonIndex]
+
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", err
+	}
+
+	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+}
+
+func decodeInteger(bencodedString string) (int, error) {
+	if bencodedString == "i-0e" {
+		return 0, fmt.Errorf("invalid integer encoding: %s", bencodedString)
+	}
+
+	if string(bencodedString[1]) == "0" && string(bencodedString[2]) != "e" {
+		return 0, fmt.Errorf("invalid integer encoding: %s", bencodedString)
+	}
+
+	var integerStr string
+	sign := 1
+	if string(bencodedString[1]) == "-" {
+		sign = -1
+		integerStr = bencodedString[2 : len(bencodedString)-1]
+	} else {
+		integerStr = bencodedString[1 : len(bencodedString)-1]
+	}
+
+	integer, err := strconv.Atoi(integerStr)
+	if err != nil {
+		return 0, err
+	}
+
+	return sign * integer, nil
 }
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	// fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	command := os.Args[1]
 
